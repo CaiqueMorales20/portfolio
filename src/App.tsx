b180473a9a63aff4bacf742d0478a5a988687a77
Caique <caiquemorales20@gmail.com>
2023-02-21 23:54:09 -0300

    :tada: First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// Imports
+import React from "react";
+
+// Functional Component
+export const App = () => {
+	return (
+		<React.Fragment>
+			<div>Ready to code!</div>
+		</React.Fragment>
+	);
+};
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+// Imports
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Imported Components
+import { App } from "./App";
+
+// Rendering
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
