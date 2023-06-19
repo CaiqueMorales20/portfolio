aaa4815aa1d60324c9abb986b9bf353b192197b9
Caique <caiquemorales20@gmail.com>
2023-06-19 19:45:18 -0300

    :heavy_plus_sign: build: @types/react-scroll

--- src/components/Header/components/NavItem/style.tsx
+++ src/components/Header/components/NavItem/style.tsx
@@ -1,6 +1,9 @@
 // Imports
 import styled from "styled-components";
 
+// Imported Components
+import { Link } from "react-scroll";
+
 // Styled Components
 export const NavItemS = styled.li`
 	position: relative;
@@ -22,10 +25,4 @@ export const NavItemS = styled.li`
 export const NavLink = styled.a`
 	color: var(--clrPrimary400);
 	font-size: var(--fzSmall);
-`;
-
-export const Teste = styled.div`
-	margin-top: 40em;
-	background-color: red;
-	height: 10rem;
 `;
