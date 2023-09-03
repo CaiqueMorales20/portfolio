fefd8a3dcefc1e7fc5f89c6cef0c3b57262735bf
Caique <caiquemorales20@gmail.com>
2023-09-03 18:00:29 -0300

    :sparkles: feat: Font Size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        sm: 'clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem);',
+        base: 'clamp(1rem, 0.34vw + 0.91rem, 1.19rem)',
+        medium: 'clamp(1.15rem, 0.61vw + 1rem, 1.38rem)',
+        lg: 'clamp(1.56rem, 1vw + 1.31rem, 2.11rem)',
+        xl: ['clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)', '1.3'],
+        '2xl': 'clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)',
+      },
     },
   },
   plugins: [],
